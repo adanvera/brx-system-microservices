30ee3166a7b8b5bdb0be09f8a933bbd479941c33
Adán Vera <gustavoadanvera@fpuna.edu.py>
2022-10-25 23:48:39 -0300

    minor fix

--- controllers/machines.js
+++ controllers/machines.js
@@ -12,7 +12,7 @@ const getMachines = async (req, res) => {
         const isToken = await checkToken(token)
         if (!isToken) return res.status(400).json({ msg: `El token no existe o ha expirado` });
 
-        const machines = await Machines.findAll({ where: { status: 1 } });
+        const machines = await Machines.findAll();
         console.log('Obtenemos los siguientes datos')
         console.log(machines.dataValues);
 
