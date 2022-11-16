c05f17cdf64df320064e1626fc5762fdc94f8b39
Adán Vera <gustavoAdnVera@fpuna.edu.py>
2022-11-15 23:49:49 -0300

    test

--- models/server.js
+++ models/server.js
@@ -33,6 +33,60 @@ class Server {
         // Directorio Público
         this.app.use(express.static('public'));
 
+        /* -------------------------------------------------------------------------- */
+
+        this.app.get("/no-cors", (req, res) => {
+            console.info("GET /no-cors");
+            res.json({
+                text: "You should not see this via a CORS request."
+            });
+        });
+
+        /* -------------------------------------------------------------------------- */
+
+        this.app.head("/simple-cors", cors(), (req, res) => {
+            console.info("HEAD /simple-cors");
+            res.sendStatus(204);
+        });
+        this.app.get("/simple-cors", cors(), (req, res) => {
+            console.info("GET /simple-cors");
+            res.json({
+                text: "Simple CORS requests are working. [GET]"
+            });
+        });
+        this.app.post("/simple-cors", cors(), (req, res) => {
+            console.info("POST /simple-cors");
+            res.json({
+                text: "Simple CORS requests are working. [POST]"
+            });
+        });
+
+        /* -------------------------------------------------------------------------- */
+
+        this.app.options("/complex-cors", cors());
+        this.app.delete("/complex-cors", cors(), (req, res) => {
+            console.info("DELETE /complex-cors");
+            res.json({
+                text: "Complex CORS requests are working. [DELETE]"
+            });
+        });
+
+        /* -------------------------------------------------------------------------- */
+
+        const issue2options = {
+            origin: true,
+            methods: ["POST"],
+            credentials: true,
+            maxAge: 3600
+        };
+        this.app.options("/issue-2", cors(issue2options));
+        this.app.post("/issue-2", cors(issue2options), (req, res) => {
+            console.info("POST /issue-2");
+            res.json({
+                text: "Issue #2 is fixed."
+            });
+        });
+
 
     }
 
