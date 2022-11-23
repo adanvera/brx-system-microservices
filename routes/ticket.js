466afe65a3c36cb57618a086a4d5deebce4ba126
Adán Vera <gustavoAdnVera@fpuna.edu.py>
2022-11-23 00:51:46 -0300

    add ticket historial

--- routes/ticket.js
+++ routes/ticket.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { getTickets, getTicketById, createTicket, modifyTicket } = require('../controllers/tickets');
+const { getTickets, getTicketById, createTicket, modifyTicket, deleteTicket } = require('../controllers/tickets');
 
 const router = Router();
 
@@ -7,5 +7,6 @@ router.post('/', createTicket)
 router.get('/', getTickets)
 router.get('/:id', getTicketById);
 router.put('/:id', modifyTicket)
+router.delete('/:id', deleteTicket)
 
 module.exports = router;
