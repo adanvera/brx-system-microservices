e0b5ff106e393c31c9ecb7cc42da6173ac254aed
Guillermo Insfran <insfranguillermo@fpuna.edu.py>
2022-12-04 14:36:23 -0300

    fix importaciones

--- controllers/importaciones.js
+++ controllers/importaciones.js
@@ -1,140 +1,56 @@
 const sequelize = require("../database/db");
 const { Importacion } = require("../helpers/querys");
+const { checkToken } = require("../helpers/verifyToken");
 const Importaciones = require('../models/importaciones')
 
 const createImportacion = async (req, res) => {
     const { token } = req.headers
-
     console.log(req.body);
-
     try {
         if (!token) return res.status(400).json({ msg: `El token es obligatorio` });
-
-        const ticket = await Importaciones.create(req.body)
-        if (ticket) {
-            return res.status(200).json({
-                ok: true,
-                content: ticket,
-                message: "Importacion creada correctamente"
-            })
-        }
+        //verificamos el token si es valido o no ha expirado
+        const isToken = await checkToken(token)
+        if (!isToken) return res.status(400).json({ msg: `El token no existe o ha expirado` });
+        const importacion = await Importaciones.create(req.body)
+        res.json(importacion)
+        console.log(importacion);
     } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            ok: false,
-            content: error,
-            message: "Error al crear la importacion"
-        })
+        return res.status(500).json({ message: error.message });
     }
 }
-
-// const getImportaciones = async (req, res) => {
-//     const { token } = req.headers
-
-//     try {
-//         if (!token) return res.status(400).json({ msg: `El token es obligatorio` });
-
-//         const importaciones = await Importaciones.findAll()
-//         if (importaciones) {
-//             return res.status(200).json({
-//                 ok: true,
-//                 content: importaciones,
-//                 message: "Importaciones obtenidas correctamente"
-//             })
-//         }
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(500).json({
-//             ok: false,
-//             content: error,
-//             message: "Error al obtener las importaciones"
-//         })
-//     }
-// }
 
 const getImportaciones = async (req, res) => {
     const { token } = req.headers
-
     try {
-
-        
-
         if (!token) return res.status(400).json({ msg: `El token es obligatorio` });
         //verificamos el token si es valido o no ha expirado
-        const [results, metadata] = await sequelize.query(
-            Importacion
-        )
-
-        if (results) {
-            return res.status(200).json({
-                ok: true,
-                content: results,
-                message: "Importaciones obtenidas correctamente"
-            })
-        }
+        const isToken = await checkToken(token)
+        if (!isToken) return res.status(400).json({ msg: `El token no existe o ha expirado` });
+        const importacion = await Importaciones.findAll();
+        res.json(importacion)
     } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            ok: false,
-            content: error,
-            message: "Error al obtener las importaciones"
-        })
+        return res.status(500).json({ message: error.message });
     }
 }
 
-const getImportacionById = async (req, res) => {
+const getImportacionesById = async (req, res) => {
     const { token } = req.headers
     const { id } = req.params
-
     try {
         if (!token) return res.status(400).json({ msg: `El token es obligatorio` });
-
-        const importacion = await Importaciones.findOne({ where: { id_importacion: id } })
-        if (importacion) {
-            return res.status(200).json({
-                ok: true,
-                content: importacion,
-                message: "Importacion obtenida correctamente"
-            })
-        }
+        //verificamos el token si es valido o no ha expirado
+        const isToken = await checkToken(token)
+        if (!isToken) return res.status(400).json({ msg: `El token no existe o ha expirado` });
+        const importacion = await Importaciones.findOne({ where: { id_importacion: id } });
+        res.json(importacion)
     } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            ok: false,
-            content: error,
-            message: "Error al obtener la importacion"
-        })
-    }
-}
-
-const modifyImportacion = async (req, res) => {
-    const { token } = req.headers
-    const { id } = req.params
-
-    try {
-        if (!token) return res.status(400).json({ msg: `El token es obligatorio` });
-
-        const importacion = await Importaciones.update(req.body, { where: { id_importacion: id } })
-        if (importacion) {
-            return res.status(200).json({
-                ok: true,
-                content: importacion,
-                message: "Importacion modificada correctamente"
-            })
-        }
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            ok: false,
-            content: error,
-            message: "Error al modificar la importacion"
-        })
+        return res.status(500).json({ message: error.message });
     }
 }
 
 module.exports = {
     createImportacion,
     getImportaciones,
-    getImportacionById,
-    modifyImportacion
+    getImportacionesById
 }
+
--- routes/importaciones.js
+++ routes/importaciones.js
@@ -1,13 +1,12 @@
 const { Router } = require('express');
-const { createImportacion, getImportaciones,
-    getImportacionById,
-    modifyImportacion } = require('../controllers/importaciones')
+const { createImportacion, getImportaciones,getImportacionesById
+} = require('../controllers/importaciones')
 
 const router = Router();
 
 router.post(`/`, createImportacion)
 router.get(`/`, getImportaciones)
-router.get(`/:id`, getImportacionById)
-router.put(`/:id`, modifyImportacion)
+router.get(`/:id`, getImportacionesById)
+
 
 module.exports = router
