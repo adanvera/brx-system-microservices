7c06db02be9cc20ba439e6ac8a71e03edd56c2b0
Adán Vera <gustavoadanvera@fpuna.edu.py>
2022-12-07 21:38:59 -0300

    se agrega eliminar maquina

--- controllers/machines.js
+++ controllers/machines.js
@@ -64,8 +64,33 @@ const getMachineById = async (req, res) => {
     }
 }
 
+const deleteMachine = async (req, res) => {
+    const { token } = req.headers
+    const { id } = req.params
+    try {
+        if (!token) return res.status(400).json({ msg: `El token es obligatorio` });
+        //verificamos el token si es valido o no ha expirado
+        const isToken = await checkToken(token)
+        if (!isToken) return res.status(400).json({ msg: `El token no existe o ha expirado` });
+
+        const machine = await Machines.destroy({
+            where: {
+                id
+            }
+        });
+
+        res.json(machine)
+        console.log('Borramos los siguientes datos');
+        console.log(machine)
+
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
 module.exports = {
     addMachine,
     getMachines,
-    getMachineById
+    getMachineById,
+    deleteMachine
 }
