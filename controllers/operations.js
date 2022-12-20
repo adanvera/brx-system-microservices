9769401e78c33c8f8ef231f652513c75d3ff1ef7
juan <marcecinco@fpuna.edu.py>
2022-12-19 23:16:54 -0300

    Agrego parametrizacion por fecha

--- controllers/operations.js
+++ controllers/operations.js
@@ -1,5 +1,5 @@
 const sequelize = require("../database/db");
-const { GET_OPERATIONS_BY_CLIENT } = require("../helpers/querys");
+const { GET_OPERATIONS_BY_CLIENT, GET_OPETARIONS_BY_DATE } = require("../helpers/querys");
 const { checkToken } = require("../helpers/verifyToken");
 const Client = require("../models/client");
 const Operation = require("../models/operation");
@@ -159,9 +159,82 @@ const extractOperations = async (req,res)=>{
 
 }
 
+const extractOperationsByDate = async (req,res)=>{
+    const { token } = req.headers
+    const { id } = req.params 
+    let {fechaDesde,fechaHasta} = req.body
+    console.log('Recibimos fecha');
+    console.log(req.body);
+    console.log("Recibimos parametros", id, token);
+    try {
+        if (!token) return res.status(400).json({ msg: `El token es obligatorio` });
+        //verificamos el token si es valido o no ha expirado
+        const isToken = await checkToken(token)
+        if (!isToken) return res.status(400).json({ msg: `El token no existe o ha expirado` });
+        
+        
+        let listExtract = []
+        fechaHasta = fechaHasta+' 00:00:00'
+        fechaDesde = fechaDesde+ ' 00:00:00'
+        const [results, metadata] = await sequelize.query(
+            GET_OPETARIONS_BY_DATE,{
+            replacements:[ Number(id),fechaDesde,fechaHasta]}
+        );
+        
+        //cambiamos el formato
+        /* results.map( op =>{
+            if(op.type === "1"){
+                op.type = "Compra"
+            }else{
+                op.type = "Venta"
+            }
+        }) */
+        results.forEach(( op )=>{
+            let extract = {
+                operation:'',
+                cliente:'',
+                compra:'',
+                venta:'',
+                comision:'',
+                moneda:'',
+                fecha:'',
+            
+            }
+            console.log(op);
+            
+            extract.fecha = op.created.toISOString().replace(/T/, ' ').      // replace T with a space
+            replace(/\..+/, '')
+            extract.moneda = "USD"
+            extract.operation = op.id_operations
+            extract.cliente = id
+            extract.comision = op.commission
+            if(op.type === "1"){
+                extract.compra = op.amount
+                extract.venta = 0
+            }else{
+                extract.compra = 0
+                extract.venta = op.amount
+            }
+            listExtract.push(extract)
+            console.log(listExtract);
+        })
+        console.log("Lista cargada");
+        console.log(listExtract);
+        res.json(listExtract)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg:'Ocurrio un error inesperado'
+        })
+    }
+        
+
+}
+
 module.exports = {
     addOperation,
     getAllOperationsByClient,
     getAllOperations,
-    extractOperations
+    extractOperations,
+    extractOperationsByDate
 }
