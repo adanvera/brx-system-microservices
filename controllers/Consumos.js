2580968cea536f9ccfe4325ca48ab15218781c82
Adán Vera <gustavoadanvera@fpuna.edu.py>
2022-12-22 11:22:25 -0300

    add

--- controllers/Consumos.js
+++ controllers/Consumos.js
@@ -4,21 +4,19 @@ const sequelize = require("../database/db");
 const { GET_CONSUMO_BY_ID } = require("../helpers/querys");
 
 const getConsumosById = async (req, res) => {
-    const { token } = req.headers
-    const { id } = req.params
+    const { id } = req.params;
     try {
-        if (!token) return res.status(400).json({ msg: `El token es obligatorio` });
-        //verificamos el token si es valido o no ha expirado
-        const isToken = await checkToken(token)
-        if (!isToken) return res.status(400).json({ msg: `El token no existe o ha expirado` });
-
-        const [results, metadata] = await sequelize.query(
-            GET_CONSUMO_BY_ID + id
-        )
-
-        res.status(200).json(results)
+        const consumos = await sequelize.query(GET_CONSUMO_BY_ID + (id), {
+            type: sequelize.QueryTypes.SELECT
+        });
+        res.status(200).json({
+            consumos
+        })
     } catch (error) {
-        return res.status(500).json({ message: error.message });
+        res.status(500).json({
+            ok: false,
+            msg: 'Error inesperado, hable con el administrador'
+        })
     }
 }
 
