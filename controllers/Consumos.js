428f5fbe90f97c8e802cf7f6b20515120c45650c
Adán Vera <gustavoadanvera@fpuna.edu.py>
2022-12-22 11:33:03 -0300

    Revert "add"
    This reverts commit 2580968cea536f9ccfe4325ca48ab15218781c82.

--- controllers/Consumos.js
+++ controllers/Consumos.js
@@ -4,19 +4,21 @@ const sequelize = require("../database/db");
 const { GET_CONSUMO_BY_ID } = require("../helpers/querys");
 
 const getConsumosById = async (req, res) => {
-    const { id } = req.params;
+    const { token } = req.headers
+    const { id } = req.params
     try {
-        const consumos = await sequelize.query(GET_CONSUMO_BY_ID + (id), {
-            type: sequelize.QueryTypes.SELECT
-        });
-        res.status(200).json({
-            consumos
-        })
+        if (!token) return res.status(400).json({ msg: `El token es obligatorio` });
+        //verificamos el token si es valido o no ha expirado
+        const isToken = await checkToken(token)
+        if (!isToken) return res.status(400).json({ msg: `El token no existe o ha expirado` });
+
+        const [results, metadata] = await sequelize.query(
+            GET_CONSUMO_BY_ID + id
+        )
+
+        res.status(200).json(results)
     } catch (error) {
-        res.status(500).json({
-            ok: false,
-            msg: 'Error inesperado, hable con el administrador'
-        })
+        return res.status(500).json({ message: error.message });
     }
 }
 
