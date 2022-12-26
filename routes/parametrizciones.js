2e9a5716dccff87f5b3da8f92aeb758a6121cd81
juan <marcecinco@fpuna.edu.py>
2022-12-26 11:58:41 -0300

    add delete params

--- routes/parametrizciones.js
+++ routes/parametrizciones.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
-const { addParams, getAllParams, updateParams } = require('../controllers/parametrizaciones');
+const { addParams, getAllParams, updateParams, deleteParam } = require('../controllers/parametrizaciones');
 
 const router = Router();
 
 router.post('/', addParams);
 router.get('/', getAllParams);
 router.put('/:codigo', updateParams);
+router.delete('/:codigo', deleteParam);
 
 module.exports = router;
